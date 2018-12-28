a04eb24b0c1426cf4c3062b0ea14de73c099f67f
ckh <2650059376@qq.com>
2018-12-28 22:37:51 +0800

    tabs页面搭建完毕

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 活动入口数据
+    activity: [
+      {
+        src: "/images/home/jifen.png",
+        text: "全职工作"
+      },
+      {
+        src: "/images/home/kanjia.png",
+        text: "兼职工作"
+      },
+      {
+        src: "/images/home/pingtuan.png",
+        text: "入职返现"
+      },
+      {
+        src: "/images/home/lingquan.png",
+        text: "爆品岗位"
+      },
+      {
+        src: "/images/home/lingquan.png",
+        text: "高薪急聘"
+      },
+    ],
   },
 
   /**
