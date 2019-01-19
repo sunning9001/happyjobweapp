d887ba82f704f896c56734081498c0234233da02
ckh <2650059376@qq.com>
2019-01-19 15:07:13 +0800

    公司介绍自定义编辑

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -67,7 +67,6 @@ Page({
         approveState,
         comName,
         reqAge, reqEducation, reqExp, reqGender, reqSkill, reqWorkYears, reqOther,
-        posComDesc,
         comCustPhone,
         retManMoney,
         fiveMoney,
@@ -87,6 +86,9 @@ Page({
       }
       if (otherWelfare){
         WxParse.wxParse('otherWelfare', 'html', otherWelfare, this);
+      }
+      if (posComDesc){
+        WxParse.wxParse('posComDesc', 'html', posComDesc, this);
       }
     })
   },  
