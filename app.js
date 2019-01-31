83fc1226e61c3588a2e7baf93743fd754db23872
ckh <2650059376@qq.com>
2019-01-31 14:22:01 +0800

    新增版本更新提示

--- app.js
+++ app.js
@@ -12,6 +12,30 @@ App({
     this.aldstat.sendEvent('小程序的启动时长', {
       time: Date.now() - startTime
     })
+    // 小程序版本更新
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+    })
+
     console.log(options)
     // let sceneArr = [1007, 1008, 1044, 1011, 1012, 1013, 1047, 1048, 1049]
     let shareToken = options.query.shareToken || null
